ddde3bd51b145c93ce7801651df5f2c81d5e302d
mohitmonga87 <mohit.monga87@gmail.com>
2025-11-10 21:27:31 -0500

    initial checkin

--- Common/obj/Debug/netstandard2.0/Common.AssemblyInfo.cs
+++ Common/obj/Debug/netstandard2.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf649fa70a1273a1ecdaff51306da1df8a195da9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

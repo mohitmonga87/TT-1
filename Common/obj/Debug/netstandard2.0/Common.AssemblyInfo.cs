e3abfb6eeebde4cf79e7e99f3451704f7e4b53fb
mohitmonga87 <mohit.monga87@gmail.com>
2025-11-10 21:32:11 -0500

    Update assembly info and cache files
    Regenerated AssemblyInfo and cache files for BusinessEntities, Common, Core, and Data projects. AssemblyInformationalVersion updated to reflect new build.

--- Common/obj/Debug/netstandard2.0/Common.AssemblyInfo.cs
+++ Common/obj/Debug/netstandard2.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddde3bd51b145c93ce7801651df5f2c81d5e302d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af433aebdecf58a7eea6266ae5788eb34c159dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
